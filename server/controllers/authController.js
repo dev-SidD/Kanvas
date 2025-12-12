2526993f49efd67d25a8ad34d3254da2af41f06e
dev-SidD <sid25jun03@gmail.com>
2025-12-12 15:17:39 +0530

    Email Verification Added

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -23,12 +23,11 @@ exports.registerUser = async (req, res) => {
     // Generate and save a verification token
     const verificationToken = crypto.randomBytes(32).toString('hex');
     user.verificationToken = verificationToken;
-    user.isVerified = true;
+
     await user.save();
 
     // Send the verification email
-    // console.log("sending email");
-    // await sendVerificationEmail(user.email, verificationToken);
+    await sendVerificationEmail(user.email, verificationToken);
 
     res.status(201).json({ msg: 'Registration successful. Please check your email to verify your account.' });
 
